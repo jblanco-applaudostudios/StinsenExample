71a9d155184b1340f304e545921e89e87ff96550
jblanco-applaudostudios <jblanco@applaudostudios.com>
2022-02-14 15:01:10 -0600

    Initial Commit

---
+++ StinsenExampleHuddle/StinsenExampleHuddleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StinsenExampleHuddleApp.swift
+//  StinsenExampleHuddle
+//
+//  Created by Josseh Blanco on 14/2/22.
+//
+
+import SwiftUI
+
+@main
+struct StinsenExampleHuddleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
